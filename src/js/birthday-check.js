50242a5256f3286021e02c2b9430201d85abdb21
NastifyMe <suprunar2010@gmail.com>
2025-08-12 19:40:01 +0300

    birthday check js some changes

---
+++ src/js/birthday-check.js
@@ -0,0 +1,19 @@
+const input_birthday = document.querySelector('.input_birthday')
+const birthday_search_btn = document.querySelector('.birthday_search_btn')
+const text_birthday = document.querySelector('#text_birthday')
+birthday_search_btn.addEventListener('click', () => {
+  const input_birthday_value = parseInt(input_birthday.value)
+  if (isNaN(input_birthday_value) || input_birthday.value.length <= 3 || input_birthday.value.length > 4){
+    text_birthday.textContent = 'Неправильний рік'
+    text_birthday.classList.add('red')
+    return
+  }
+
+  if ((input_birthday_value % 4 === 0 && input_birthday_value % 100 !== 0) || input_birthday_value % 400 === 0){
+    text_birthday.textContent = 'Ви народилися у високосний рік!'
+    text_birthday.className = "green";
+  } else {
+    text_birthday.textContent = 'Ви народилися не у високосний рік!'
+    text_birthday.className = "red";
+  }
+})
