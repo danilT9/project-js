9ef31c8572587896d313800a9f3e04c31843b188
danilT <daniltrotsko9@gmail.com>
2025-08-19 12:05:07 +0300

    header

--- src/js/header.js
+++ src/js/header.js
@@ -6,11 +6,11 @@ const userHello = document.getElementById("user-hello");
 
 welcomeModal.addEventListener("submit", (e) => {
     e.preventDefault()
-    if (nameInput.value.length >= 6 && nameInput.value.length <= 16) {
+    if (nameInput.value.length >= 3 && nameInput.value.length <= 16) {
         userHello.textContent = "Вітаємо, " + nameInput.value;
         welcomeModal.style.display = "none";
     } else {
-        nameInput.placeholder = "Ім'я повинне бути більше 6 символів і менше 16!"
+        nameInput.placeholder = "Ім'я повинне бути більше 3 символів і менше 16!"
         nameInput.style.color = "red";
         nameInput.value = ""
     }
